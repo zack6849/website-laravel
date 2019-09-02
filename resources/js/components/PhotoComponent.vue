d8c8ded2300abd1213fa605274e15a3bea7b73ce
zack6849 <zack@zack6849.com>
2019-09-02 11:05:33 -0400

    Move clickable element to just be the image Only show tags if there actually are tags

--- resources/js/components/PhotoComponent.vue
+++ resources/js/components/PhotoComponent.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="photo-container m-4">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer" @click="openFlickr">
-            <img class="w-full" v-bind:src="info.url_m" v-bind:alt="info.title">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img  @click="openFlickr" class="w-full cursor-pointer" v-bind:src="info.url_m" v-bind:alt="info.title">
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2" v-text="info.title"></div>
             </div>
             <div class="px-6 py-4">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700" v-for="tag in info.tags.split(' ')" v-text="tag"></span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 mb-1 text-sm font-semibold text-gray-700" v-if="info.tags.length > 0" v-for="tag in info.tags.split(' ')" v-text="tag"></span>
             </div>
         </div>
     </div>
