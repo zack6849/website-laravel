613d613e519dfb7e5d26e9a706d5ac233a8a0efa
Zachary Craig <zack@zack6849.com>
2019-09-08 14:35:42 -0400

    Update envoy
    Todo: can't actually restart fpm from envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -14,38 +14,53 @@ $deployment = $builds . '/' . $date;
 $serve = $appDir . '/source';
 $env = $appDir . '/.env';
 $storage = $appDir . '/storage';
+$use_fpm = true;
+$fpm_service = 'php7.3-fpm';
 @endsetup
 
 @story('deploy')
 	git
 	install
 	live
+    @if ($use_fpm)
+        restart-fpm
+    @endif
 @endstory
 
 @task('git', ['on' => 'production'])
+    echo "Cloning..."
 	git clone -b {{ $branch }} "{{ $repo }}" {{ $deployment }}
 @endtask
 
 @task('install', ['on' => 'production'])
+echo "Installing new version"
 cd {{ $deployment }}
-
 rm -rf {{ $deployment }}/storage
-
 ln -nfs {{ $env }} {{ $deployment }}/.env
-
 ln -nfs {{ $storage }} {{ $deployment }}/storage
+echo "Installing NPM dependencies"
 npm install
-composer install --prefer-dist
+echo "Installing PHP dependencies"
+composer install --no-interaction --quiet --no-dev --prefer-dist --optimize-autoloader
+echo "Compiling SCSS & JS"
 npm run production
+echo "Running migrations"
 php ./artisan migrate --force
-echo "Deployment complete!"
-echo "You may need to restart PHP-FPM if it's being used"
 @endtask
 
 @task('live', ['on' => 'production'])
+echo "Making current deployment live"
 cd {{ $deployment }}
-
 ln -nfs {{ $deployment }} {{ $serve }}
+echo "Clearing caches"
+php artisan view:clear --quiet
+php artisan cache:clear --quiet
+php artisan config:clear --quiet
+echo "Deployment complete!"
+@endtask
+
+@task('restart-fpm', ['on' => 'production', 'confirm' => 'Restart PHP-FPM?'])
+    sudo service {{$fpm_service}} restart
 @endtask
 
 
