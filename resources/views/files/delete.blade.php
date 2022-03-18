79e2b95988972759161789a3293851af3207681e
Zachary Craig <zack@zcraig.me>
2022-03-18 02:10:41 -0400

    clean up the delete view a bit.

--- resources/views/files/delete.blade.php
+++ resources/views/files/delete.blade.php
@@ -1,11 +1,10 @@
 <x-base-page title="Delete File?">
-    <h1>Are you sure?</h1>
-    You're about to delete file <b>{{$file->original_filename}}</b>, this action cannot be undone.
-
+    <p>You're about to delete file <b>{{$file->original_filename}}</b>, this action cannot be undone.</p>
+    <h1 class="pb-5">Are you sure?</h1>
     <form action="{{route('file.destroy', $file)}}" method="post">
         {{method_field('post')}}
         {{csrf_field()}}
-        <button type="submit">Delete File</button>
-        <button type="button">Never mind.</button>
+            <button type="submit" class="bg-red-600 rounded py-4 px-2 text-white leading-none shadow mr-4">Delete File</button>
+            <button type="button" class="bg-purple-600 rounded py-4 px-2 text-white leading-none shadow">Never mind.</button>
     </form>
 </x-base-page>
