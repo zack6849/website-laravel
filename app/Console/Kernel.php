1842b8160540d414fbe285f57714ea0b787b675c
Zachary Craig <zack@zcraig.me>
2023-03-20 17:48:53 -0400

    Run QRZ logbook import daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('upload:scan')->everyMinute();
+        $schedule->command('logbook:import')->daily();
     }
 
     /**
